c9abf4126b9b688436c6e9654510e44e7886c05e
Ibrahim Olanigan <salalite@gmail.com>
2023-11-11 23:03:08 -0500

    Updated Inference pipeline

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 install:
 	pip install -r requirements.txt
 
+undo:
+	pip uninstall -f -r requirements.txt --no-input
+
+run:
+	./src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+
+all: install run
--- src/01-module/scripts/run-feature-and-prediction-pipelines.sh
+++ src/01-module/scripts/run-feature-and-prediction-pipelines.sh
@@ -5,5 +5,5 @@ set -e
 cd src/01-module
 
 jupyter nbconvert --to notebook --execute iris-feature-pipeline.ipynb
-jupyter nbconvert --to notebook --execute iris-batch-inference-pipeline.ipynb
+jupyter nbconvert --to notebook --execute iris-inference-pipeline.ipynb
 
